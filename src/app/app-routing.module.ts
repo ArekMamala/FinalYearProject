7edd83e7d378c28ea84e585b1aff83417ec403ab
SammarTahir <G00347526@gmit.ie>
2020-01-13 21:51:09 +0000

    updates for others to git pull .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: "",
-    loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
-  },
+  
+  { path: '', redirectTo: 'developers', pathMatch: 'full' },
   {
     path: 'developers',
     loadChildren: () => import('./pages/developers/developers.module').then( m => m.DevelopersPageModule)
