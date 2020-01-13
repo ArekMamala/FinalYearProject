8bffa56edf99fed8dd79c624d85bdb4380ed5746
SammarTahir <G00347526@gmit.ie>
2020-01-13 20:26:49 +0000

    Attempting SQL database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SQLite, SQLiteObject } from "@ionic-native/sqlite/ngx";
+import { DatabaseService } from '../services/database.service';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(private db: DatabaseService){ }
 
+
+
+
+  ngOnInit() {
+    this.db.getDatabaseState().subscribe(ready => {
+      if (ready){
+      
+      }
+    })
+  }
+  
 }
