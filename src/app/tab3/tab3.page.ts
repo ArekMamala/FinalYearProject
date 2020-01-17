5eb187e972783b3733ad97c12ee6382065c4bb32
ArekMamala <G00349088@gmit.ie>
2020-01-17 22:48:07 +0000

    Geolocation installed

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion/ngx';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { BackgroundGeolocation} from '@ionic-native/background-geolocation';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,11 @@ import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOp
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+
+
+  public lat;
+  public lng;
+  public speed;
 
   x:number;
   y:number;
@@ -20,7 +28,7 @@ export class Tab3Page {
 
   id: any;
 
-  constructor(public deviceMotion: DeviceMotion) { 
+  constructor(public deviceMotion: DeviceMotion, /*Geolocation: Geolocation,  public backgroundGeolocation: BackgroundGeolocation*/) { 
     this.x = 0;
     this.y = 0;
     this.z = 0;
@@ -30,6 +38,54 @@ export class Tab3Page {
     this.zStart = 0;
     this.punch = 0 ;
   }
+
+
+
+
+
+/*
+  startTracking() {
+
+    let config = {
+      desiredAccuracy: 0,
+      stationaryRadius: 5,
+      distanceFilter: 10,
+      debug: true,
+      interval: 1000
+    };
+
+    this.backgroundGeolocation.configure(config).subscribe((location) => {
+
+    //console.log('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
+//      console.log(this.lat ,      this.lng,      this.speed);
+
+    this.zone.run(() => {
+      this.lat = location.latitude;
+      this.lng = location.longitude;
+      this.speed = (location.speed * 3600)/1000 ; // can be speed * 3.6 and should be round for 2 decimal
+      
+    });
+
+  }, (err) => {
+    console.log(err);
+
+  });
+
+  this.backgroundGeolocation.start();
+
+  let options = {
+    frequency: 1000,
+    enableHighAccuracy: true
+  };
+}
+*/
+
+
+
+
+
+
+
 
   start(){
     try {
