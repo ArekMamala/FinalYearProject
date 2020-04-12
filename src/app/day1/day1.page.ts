93e7a89b37c204e55e1ff90dd734e5c1d73d3107
Usman Sattar <G00345816@gmit.ie>
2020-04-12 13:58:39 +0100

    Server Connected To Local Database

--- src/app/day1/day1.page.ts
+++ src/app/day1/day1.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-day1',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day1.page.scss'],
 })
 export class Day1Page implements OnInit {
+  posts: any = [];
 
-  constructor() { }
+  constructor(private api: PostService, private router: Router) { }
 
   ngOnInit() {
+    this.api.getPostsData().subscribe(data => {
+      this.posts = data;
+      console.log(this.posts)
+    });
   }
 
 }
