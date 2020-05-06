b3af682073be4d01767fdbd6434b98fce76cdf21
Arek Mamala <mamala132@gmail.com>
2020-05-06 01:53:35 +0100

    Icon & Splashscreen generated for the application routing fixed in workouts and meals

--- src/app/meals/meals.page.ts
+++ src/app/meals/meals.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-meals',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MealsPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
-
+  
+  home(){
+    this.router.navigate(['/home/applicationPage'])
+  }
 }
